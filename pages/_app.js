3a0804fec0bf060416c2e65e534d295c0811bb61
James Dimonaco <dimonaco.james@gmail.com>
2021-06-10 19:45:38 +0100

    i've broken toast into it's own component

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
-import '../styles/globals.css'
-import Layout from '../src/components/layout/Layout'
-import Sidebar from '../src/components/sidebar/Sidebar'
-import { Transition } from '@headlessui/react'
-import { Fragment, useState } from 'react'
-import { useTimeoutFn } from 'react-use'
+import "../styles/globals.css";
+import Layout from "../src/components/layout/Layout";
+import Sidebar from "../src/components/sidebar/Sidebar";
+import { Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { useTimeoutFn } from "react-use";
 
 function MyApp({ Component, pageProps }) {
-  let [isShowing, setIsShowing] = useState(true)
-  let [, , resetIsShowing] = useTimeoutFn(() => setIsShowing(true), 500)
+  let [isShowing, setIsShowing] = useState(true);
+  let [resetIsShowing] = useTimeoutFn(() => setIsShowing(true), 500);
   return (
     <Layout>
       <Sidebar>
         <Component {...pageProps} />
       </Sidebar>
     </Layout>
-
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
